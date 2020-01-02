4cf147f63e4edef2323451ecee1c350a0aae166b
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-01-02 19:32:51 +0900

    background & filter input value
    1. Add backgound picture
    (The picture was taken by me)
    2. If user's input value is equal with '',
    the list will not loaded with it.

--- main.js
+++ main.js
@@ -17,45 +17,45 @@ function clickHandler(event) {
     event.preventDefault();
     const ET = event.target;
     const ETC = ET.classList;
-    const ETspan=ET.parentElement.children[2];
+    const ETspan = ET.parentElement.children[2];
     const ETspanId = ETspan.id;
-    if(ETC.contains('emptyButton')){
+    if (ETC.contains('emptyButton')) {
         const next = ET.nextElementSibling.classList;
         ETC.toggle('dis-show')
         ETC.toggle('dis-non')
         next.toggle('dis-show')
         next.toggle('dis-non')
-        ETspan.style.textDecoration='line-through'
-        liEle_LS[ETspanId-1].isChecked=true;
+        ETspan.style.textDecoration = 'line-through'
+        liEle_LS[ETspanId - 1].isChecked = true;
         saveLS();
-    }else if(ETC.contains('checkedButton')){
+    } else if (ETC.contains('checkedButton')) {
         const previous = ET.previousElementSibling.classList;
         ETC.toggle('dis-show')
         ETC.toggle('dis-non')
         previous.toggle('dis-show')
         previous.toggle('dis-non')
-        ETspan.style.textDecoration='none'
-        liEle_LS[ETspanId-1].isChecked=false;
+        ETspan.style.textDecoration = 'none'
+        liEle_LS[ETspanId - 1].isChecked = false;
         saveLS();
-    }else if(ETC.contains('removeButton')){
+    } else if (ETC.contains('removeButton')) {
         ET.parentElement.remove();
-        liEle_LS.splice(ETspanId-1,1);
-        liEle_LS.forEach((li,index)=>li.id=index+1);
+        liEle_LS.splice(ETspanId - 1, 1);
+        liEle_LS.forEach((li, index) => li.id = index + 1);
         saveLS();
     }
 }
 function saveLS() {
     localStorage.setItem(TODOS, JSON.stringify(liEle_LS))
 }
-function createNewList(IV){
+function createNewList(IV) {
     const li = document.createElement('li');
-    li.innerHTML =`<i class="fa fa-square-o item btn emptyButton dis-show" aria-hidden="true"></i>
+    li.innerHTML = `<i class="fa fa-square-o item btn emptyButton dis-show" aria-hidden="true"></i>
     <i class="fa fa-check-square-o item btn checkedButton dis-non" aria-hidden="true"></i>
-    <span class='item' id = '${liEle_LS.length+1}'>${IV}</span>
+    <span class='item' id = '${liEle_LS.length + 1}'>${IV}</span>
     <i class="fa fa-trash-o btn removeButton item" aria-hidden="true"></i>`;
-    li.addEventListener('click',clickHandler)
+    li.addEventListener('click', clickHandler)
     ul.appendChild(li)
-    input.value="";
+    input.value = "";
     const liEle = {
         text: IV,
         id: liEle_LS.length + 1,
@@ -68,8 +68,9 @@ function keyupHandler(event) {
     event.preventDefault()
     if (event.keyCode == 13) {
         const IV = input.value;
-        createNewList(IV);
-        
+        if (IV) {
+            createNewList(IV);
+        }
     }
 }
 function init() {
@@ -79,7 +80,7 @@ function init() {
 
     if (getLS) {
         const parsedLS = JSON.parse(getLS);
-        parsedLS.forEach(ls=>createNewList(ls.text));
+        parsedLS.forEach(ls => createNewList(ls.text));
     }
 }
 init();
