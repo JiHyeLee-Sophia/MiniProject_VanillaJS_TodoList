2120b2af30945d99606f78c99bb83d965d76c331
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-01-04 23:13:17 +0900

    Add an event and message
    Add contextmenu event to prevent mouse right click button.
    Add a message for picture sourse on the bottom of page

--- background.js
+++ background.js
@@ -7,4 +7,8 @@ function init(){
     const backG = document.querySelector('body');
     backG.style.background = `#333 url('img/${number}.jpg') no-repeat center center / cover`;
 }
+function rightClickHandler(event){
+    event.preventDefault();
+}
+document.addEventListener('contextmenu', rightClickHandler)
 init();
