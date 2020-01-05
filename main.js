a20e4587f37ba55e4bd3ae7e9d9bd36c48f38565
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-01-05 19:41:07 +0900

    Fix an error
    Fix an error of after refresh the screen, list doesn't show up as checked box after click empty check box

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const TODOS = 'todos';
 const container = document.querySelector('.container');
 const today = container.querySelector('.date');
-const getLS = localStorage.getItem(TODOS);
 const btns = document.querySelectorAll('.btn');
 const ul = document.querySelector('.todoList')
 const input = document.querySelector('.input')
+const getLS = localStorage.getItem(TODOS);
 let liEle_LS = [];
 
 function getDate() {
@@ -21,18 +21,14 @@ function clickHandler(event) {
     const ETspanId = ETspan.id;
     if (ETC.contains('emptyButton')) {
         const next = ET.nextElementSibling.classList;
-        ETC.toggle('dis-show')
         ETC.toggle('dis-non')
-        next.toggle('dis-show')
         next.toggle('dis-non')
         ETspan.style.textDecoration = 'line-through'
         liEle_LS[ETspanId - 1].isChecked = true;
         saveLS();
     } else if (ETC.contains('checkedButton')) {
         const previous = ET.previousElementSibling.classList;
-        ETC.toggle('dis-show')
         ETC.toggle('dis-non')
-        previous.toggle('dis-show')
         previous.toggle('dis-non')
         ETspan.style.textDecoration = 'none'
         liEle_LS[ETspanId - 1].isChecked = false;
@@ -49,7 +45,7 @@ function saveLS() {
 }
 function createNewList(IV) {
     const li = document.createElement('li');
-    li.innerHTML = `<i class="fa fa-square-o item btn emptyButton dis-show" aria-hidden="true"></i>
+    li.innerHTML = `<i class="fa fa-square-o item btn emptyButton" aria-hidden="true"></i>
     <i class="fa fa-check-square-o item btn checkedButton dis-non" aria-hidden="true"></i>
     <span class='item' id = '${liEle_LS.length + 1}'>${IV}</span>
     <i class="fa fa-trash-o btn removeButton item" aria-hidden="true"></i>`;
@@ -60,6 +56,19 @@ function createNewList(IV) {
         text: IV,
         id: liEle_LS.length + 1,
         isChecked: false
+    }
+    if (getLS) {
+        const newLS = JSON.parse(getLS);
+        newLS.forEach(ls => {
+            if (ls.id === liEle.id) {
+                liEle.isChecked = ls.isChecked;
+                if (ls.isChecked) {
+                    li.children[2].style.textDecoration = 'line-through';
+                    li.firstElementChild.classList.add('dis-non');
+                    li.firstElementChild.nextElementSibling.classList.remove('dis-non')
+                }
+            }
+        })
     }
     liEle_LS.push(liEle);
     saveLS();
@@ -77,7 +86,6 @@ function init() {
     getDate();
     btns.forEach(btn => btn.addEventListener('click', clickHandler));
     document.addEventListener('keyup', keyupHandler)
-
     if (getLS) {
         const parsedLS = JSON.parse(getLS);
         parsedLS.forEach(ls => createNewList(ls.text));
