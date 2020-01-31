09774a83b205ee0302cc37b47176f5efc4df605f
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-02-01 02:12:47 +0900

    Update as OOP
    Update from functional programming to using class

--- background.js
+++ background.js
@@ -1,17 +1,31 @@
+const body = document.querySelector("body");
 
-function getNumber(){
-    const randomNum = Math.floor(Math.random()*77);
-    return randomNum;
-}
-function init(){
-    const number = getNumber();
-    const backG = document.querySelector('body');
-    backG.style.background = `#333 url('img/${number}.jpg') no-repeat center center / cover`;
-    backG.style.backgroundAttachment='fixed'
-}
-function rightClickHandler(event){
+class background {
+  constructor(number) {
+    this.number = number;
+  }
+  preventRightClick() {
+    document.addEventListener("contextmenu", this.rightClickHandler);
+  }
+  rightClickHandler(event) {
     event.preventDefault();
+  }
+  setRandomNumber(){
+    const random = Math.floor(Math.random() * 77);
+    const bg = new background(random);
+    bg.setBackground(); 
+  }
+  setBackground() {
+    this.preventRightClick();
+    body.style.background = `#333 url('img/${this.number}.jpg') no-repeat center center / cover`;
+    body.style.backgroundAttachment = "fixed";
+    setTimeout(()=>{
+        this.setRandomNumber()
+    },1000)
+  }
 }
-document.addEventListener('contextmenu', rightClickHandler)
-init();
-setInterval(() =>init(), 300000);
+
+const random = Math.floor(Math.random() * 77);
+const bg = new background(random);
+bg.setBackground();
+
